483f0dbf29555c971129c1ee0a1b456ba06b91dd
alekalek <alek@spectra-alarm.com>
2020-11-05 10:01:45 +0100

    added backend spring application architecture and dependencies.

---
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/CulturalHeritageApplication.java
@@ -0,0 +1,13 @@
+package tim2.CulturalHeritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalHeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalHeritageApplication.class, args);
+	}
+
+}
---
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/CulturalHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package tim2.CulturalHeritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
