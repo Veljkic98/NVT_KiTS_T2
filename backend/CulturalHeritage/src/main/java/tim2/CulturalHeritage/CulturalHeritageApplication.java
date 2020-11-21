f8b6708702fc7e9eb0c7c8f0d969d2119d11d0cd
alekalek <alek@spectra-alarm.com>
2020-11-21 11:30:52 +0100

    disabled spring security and added CORS policy

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/CulturalHeritageApplication.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/CulturalHeritageApplication.java
@@ -2,12 +2,29 @@ package tim2.CulturalHeritage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+	}
+	)
 public class CulturalHeritageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CulturalHeritageApplication.class, args);
+	}
+
+	// CORS for frontend
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
