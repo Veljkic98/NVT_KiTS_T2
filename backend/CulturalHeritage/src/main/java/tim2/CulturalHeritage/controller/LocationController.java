4843505c5cc979bc2df2e8ff593e9619a729fc96
alekalek <alek@spectra-alarm.com>
2020-11-21 11:13:47 +0100

    added annotations for rest controllers and end points (paths)

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/controller/LocationController.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/controller/LocationController.java
@@ -1,9 +1,13 @@
 package tim2.CulturalHeritage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tim2.CulturalHeritage.service.LocationService;
 
+@RestController
+@RequestMapping("/api/location")
 public class LocationController {
 
     @Autowired
--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/controller/NewsController.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/controller/NewsController.java
@@ -1,9 +1,13 @@
 package tim2.CulturalHeritage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tim2.CulturalHeritage.service.NewsService;
 
+@RestController
+@RequestMapping("/api/news")
 public class NewsController {
 
     @Autowired
