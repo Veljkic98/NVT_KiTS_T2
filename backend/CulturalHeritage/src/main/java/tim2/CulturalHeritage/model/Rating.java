3a97c9c8af3a805d1a88a43689577ec7a7f1cbc7
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-11-28 16:01:45 +0100

    added empty constructor into Rating and Comment

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/model/Rating.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/model/Rating.java
@@ -25,6 +25,9 @@ public class Rating {
     @ManyToOne
     private AuthenticatedUser authenticatedUser;
 
+    public Rating() {
+    }
+
     public Rating(Long id, int grade, CulturalHeritage culturalHeritage, AuthenticatedUser authenticatedUser) {
         this.id = id;
         this.grade = grade;
