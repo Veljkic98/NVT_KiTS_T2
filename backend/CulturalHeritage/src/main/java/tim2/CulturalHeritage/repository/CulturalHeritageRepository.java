f078d28cd8f5777c02611235ef7ceb0068771b43
unknown <perovicmitar@gmail.com>
2020-12-22 16:37:17 +0100

    Added filters for cusbtype and location city and country for Cultural Heritae

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/CulturalHeritageRepository.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/CulturalHeritageRepository.java
@@ -1,11 +1,21 @@
 package tim2.CulturalHeritage.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import tim2.CulturalHeritage.model.CulturalHeritage;
 
 @Repository
 public interface CulturalHeritageRepository extends JpaRepository<CulturalHeritage, Long> {
+
+    Page<CulturalHeritage> findByChsubtypeName();
+
+    Page<CulturalHeritage> findByLocation_City();
+
+    Page<CulturalHeritage> findByLocation_Country();
+
+
 
 }
