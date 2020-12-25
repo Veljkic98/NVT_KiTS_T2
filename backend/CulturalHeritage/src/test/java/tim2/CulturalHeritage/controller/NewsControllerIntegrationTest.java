ca4e20ccd2f18a60d7833a41e29a1d1931e77d86
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-25 23:00:30 +0100

    code refactored

--- backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/controller/NewsControllerIntegrationTest.java
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/controller/NewsControllerIntegrationTest.java
@@ -12,14 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.TestPropertySource;
@@ -30,29 +28,17 @@ import tim2.CulturalHeritage.dto.requestDTO.AuthUserLoginDTO;
 import tim2.CulturalHeritage.dto.requestDTO.NewsRequestDTO;
 import tim2.CulturalHeritage.dto.responseDTO.AuthUserLoginResponseDTO;
 import tim2.CulturalHeritage.dto.responseDTO.NewsResponseDTO;
-import tim2.CulturalHeritage.model.Admin;
 import tim2.CulturalHeritage.service.NewsService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static tim2.CulturalHeritage.constants.NewsConstants.*;
 import static tim2.CulturalHeritage.constants.LoginConstants.*;
 
-
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import tim2.CulturalHeritage.dto.responseDTO.NewsResponseDTO;
 import tim2.CulturalHeritage.model.News;
 import tim2.CulturalHeritage.restTemplateHelp.RestResponsePage;
-import tim2.CulturalHeritage.service.NewsService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -65,12 +51,10 @@ public class NewsControllerIntegrationTest {
   @Autowired
   private NewsService newsService;
 
-  
   @Test
-  public void findById_ValidId_ShouldReturnNews(){
+  public void findById_ValidId_ShouldReturnNews() {
 
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-      restTemplate.getForEntity("/api/news/1", NewsResponseDTO.class);
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.getForEntity("/api/news/1", NewsResponseDTO.class);
 
     NewsResponseDTO newsResponseDTO = responseEntity.getBody();
     assertEquals("Naslov1", newsResponseDTO.getHeading());
@@ -78,69 +62,68 @@ public class NewsControllerIntegrationTest {
   }
 
   @Test
-  public void findById_InvalidId_ShouldReturnNotFound(){
+  public void findById_InvalidId_ShouldReturnNotFound() {
 
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-      restTemplate.getForEntity("/api/news/100", NewsResponseDTO.class);
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.getForEntity("/api/news/100", NewsResponseDTO.class);
 
     NewsResponseDTO newsResponseDTO = responseEntity.getBody();
     assertNull(newsResponseDTO);
     assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
   }
 
-
-  
   public HttpHeaders login() {
+
     ResponseEntity<AuthUserLoginResponseDTO> responseEntity = restTemplate.postForEntity("/auth/login",
-            new AuthUserLoginDTO(ADMIN_EMAIL, ADMIN_PASS), AuthUserLoginResponseDTO.class);
+        new AuthUserLoginDTO(ADMIN_EMAIL, ADMIN_PASS), AuthUserLoginResponseDTO.class);
 
     String accessToken = "Bearer " + responseEntity.getBody().getAccessToken();
 
     HttpHeaders authHeaders = new HttpHeaders();
     authHeaders.add("Authorization", accessToken);
-    //auth headers cant be json because of a file
+    // auth headers cant be json because of a file
     // authHeaders.setContentType(MediaType.APPLICATION_JSON);
     return authHeaders;
-}
+  }
 
-  private HttpEntity<LinkedMultiValueMap<String, Object>> createFormData( NewsRequestDTO newsRequestDTO, String path){
+  private HttpEntity<LinkedMultiValueMap<String, Object>> createFormData(NewsRequestDTO newsRequestDTO, String path) {
 
-    //Dto is param -> body -> form data
-    // set JSON content type for dto. 
+    // Dto is param -> body -> form data
+    // set JSON content type for dto.
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity<NewsRequestDTO> dto = new HttpEntity<>(newsRequestDTO, headers);
 
-    //params map is corresponding to form data when sending request
-    //params map should contains file (multipart file) and  news dto as json
+    // params map is corresponding to form data when sending request
+    // params map should contains file (multipart file) and news dto as json
     LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
 
-    //add multipart file to params (to form data when sending request) if file is passed
-    if(null == path || "".equals(path)){
+    // add multipart file to params (to form data when sending request) if file is
+    // passed
+    if (null == path || "".equals(path)) {
       params.add("file", null);
-     }
-    else{
+    } else {
       params.add("file", new FileSystemResource(path));
     }
-      
-    //add dto obj to params (to form data when sending request)
+
+    // add dto obj to params (to form data when sending request)
     params.add("news", dto);
 
-     //add authentication headers when sending request (add admin)
+    // add authentication headers when sending request (add admin)
     HttpHeaders headersAuth = login();
     return new HttpEntity<>(params, headersAuth);
   }
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void add_WithFile_ShouldReturnNews(){
+  public void add_WithFile_ShouldReturnNews() {
+
     NewsRequestDTO newsRequestDTO = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
     String imgPath = "src/test/resources/cultural-heritage-management.jpg";
 
     HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = createFormData(newsRequestDTO, imgPath);
-    
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-      restTemplate.postForEntity("/api/news", requestEntity, NewsResponseDTO.class);
+
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.postForEntity("/api/news", requestEntity,
+        NewsResponseDTO.class);
 
     NewsResponseDTO newsResponseDTO = responseEntity.getBody();
     assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
@@ -149,27 +132,29 @@ public class NewsControllerIntegrationTest {
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void add_WithoutFile_ShouldBadRequest(){
-    NewsRequestDTO newsRequestDTO  = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
+  public void add_WithoutFile_ShouldBadRequest() {
+
+    NewsRequestDTO newsRequestDTO = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
 
     HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = createFormData(newsRequestDTO, "");
 
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-      restTemplate.postForEntity("/api/news", requestEntity, NewsResponseDTO.class);
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.postForEntity("/api/news", requestEntity,
+        NewsResponseDTO.class);
 
     assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
   }
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void update_ValidID_ShouldReturnNews(){
-    NewsRequestDTO newsRequestDTO  = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
+  public void update_ValidID_ShouldReturnNews() {
+
+    NewsRequestDTO newsRequestDTO = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
     String imgPath = "src/test/resources/cultural-heritage-management.jpg";
 
     HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = createFormData(newsRequestDTO, imgPath);
 
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-    restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.PUT ,requestEntity, NewsResponseDTO.class);
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.PUT,
+        requestEntity, NewsResponseDTO.class);
 
     NewsResponseDTO newsResponseDTO = responseEntity.getBody();
     assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
@@ -178,87 +163,88 @@ public class NewsControllerIntegrationTest {
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void update_InvalidID_ShouldReturnNotFound(){
-    NewsRequestDTO newsRequestDTO  = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
+  public void update_InvalidID_ShouldReturnNotFound() {
+
+    NewsRequestDTO newsRequestDTO = new NewsRequestDTO(null, HEADING, CONTENT, 1, 1);
     String imgPath = "src/test/resources/cultural-heritage-management.jpg";
 
     HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = createFormData(newsRequestDTO, imgPath);
 
-    ResponseEntity<NewsResponseDTO> responseEntity = 
-    restTemplate.exchange("/api/news/" + NEWS_ID_NOT_FOUND, HttpMethod.PUT ,requestEntity, NewsResponseDTO.class);
+    ResponseEntity<NewsResponseDTO> responseEntity = restTemplate.exchange("/api/news/" + NEWS_ID_NOT_FOUND,
+        HttpMethod.PUT, requestEntity, NewsResponseDTO.class);
 
     assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
   }
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void delete_NotLoggedIn_ShouldReturnUNAUTHORIZED(){
+  public void delete_NotLoggedIn_ShouldReturnUNAUTHORIZED() {
 
-    ResponseEntity<Void> responseEntity = 
-      restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.DELETE, null ,Void.class );
-    
+    ResponseEntity<Void> responseEntity = restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.DELETE, null,
+        Void.class);
+
     assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode());
   }
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void delete_LoggedInValidID_ShouldDelete(){
+  public void delete_LoggedInValidID_ShouldDelete() {
+
     HttpHeaders authHeaders = login();
     HttpEntity<Object> requestEntity = new HttpEntity<Object>(null, authHeaders);
 
-    ResponseEntity<Void> responseEntity = 
-    restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.DELETE, requestEntity ,Void.class );
-  
-  assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
+    ResponseEntity<Void> responseEntity = restTemplate.exchange("/api/news/" + NEWS_ID, HttpMethod.DELETE,
+        requestEntity, Void.class);
+
+    assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
 
   }
 
   @Test
   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
-  public void delete_InvalidID_ShouldNotDelete(){
+  public void delete_InvalidID_ShouldNotDelete() {
+
     HttpHeaders authHeaders = login();
     HttpEntity<Object> requestEntity = new HttpEntity<Object>(null, authHeaders);
 
-    ResponseEntity<Void> responseEntity = 
-    restTemplate.exchange("/api/news/" + NEWS_ID_NOT_FOUND, HttpMethod.DELETE, requestEntity ,Void.class );
-  
-  assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    ResponseEntity<Void> responseEntity = restTemplate.exchange("/api/news/" + NEWS_ID_NOT_FOUND, HttpMethod.DELETE,
+        requestEntity, Void.class);
+
+    assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
   }
 
+  @Test
+  public void findAllForCH_chIdOk_listAndOk() {
 
+    Pageable pageable = PageRequest.of(0, PAGE_SIZE);
+    Page<News> newsPage = newsService.findAll(pageable, CH_ID);
+    List<News> newsList = newsPage.getContent();
 
-    @Test
-    public void findAllForCH_chIdOk_listAndOk() {
+    int size = newsList.size();
 
-        Pageable pageable = PageRequest.of(0, PAGE_SIZE);
-        Page<News> newsPage = newsService.findAll(pageable, CH_ID);
-        List<News> newsList = newsPage.getContent();
+    ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>> responseType = new ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>>() {
+    };
 
-        int size = newsList.size();
+    ResponseEntity<RestResponsePage<NewsResponseDTO>> responseEntity = restTemplate
+        .exchange("/api/news/by-page/1/?page=0&size=5&sort=id,ASC", HttpMethod.GET, null/* httpEntity */, responseType);
 
-        ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>> responseType = 
-        new ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>>() {};
+    List<NewsResponseDTO> responseList = responseEntity.getBody().getContent();
 
-        ResponseEntity<RestResponsePage<NewsResponseDTO>> responseEntity = restTemplate.exchange(
-                "/api/news/by-page/1/?page=0&size=5&sort=id,ASC", HttpMethod.GET, null/* httpEntity */, responseType);
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertEquals(responseList.size(), size);
+  }
 
-        List<NewsResponseDTO> responseList = responseEntity.getBody().getContent();
+  @Test
+  public void findAllForCH_chIdNotExists_listAndOk() {
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(responseList.size(), size);
-    }
+    ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>> responseType = new ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>>() {
+    };
 
-    @Test
-    public void findAllForCH_chIdNotExists_listAndOk() {
+    ResponseEntity<RestResponsePage<NewsResponseDTO>> responseEntity = restTemplate.exchange(
+        "/api/news/by-page/" + CH_ID_NOT_EXISTS + "/?page=0&size=5&sort=id,ASC", HttpMethod.GET, null/* httpEntity */,
+        responseType);
 
-        ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>> responseType = 
-        new ParameterizedTypeReference<RestResponsePage<NewsResponseDTO>>() {};
-
-        ResponseEntity<RestResponsePage<NewsResponseDTO>> responseEntity = restTemplate.exchange(
-                "/api/news/by-page/" + CH_ID_NOT_EXISTS + "/?page=0&size=5&sort=id,ASC", HttpMethod.GET, null/* httpEntity */, responseType);
-
-
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-    }
+    assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+  }
 
 }
