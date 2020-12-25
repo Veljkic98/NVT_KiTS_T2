45c98329cb12a321efb86944a3f9d53f98a26d0e
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-25 13:13:04 +0100

    deleted unnecessary imports

--- backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/controller/CHTypeControllerIntegrationTest.java
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/controller/CHTypeControllerIntegrationTest.java
@@ -2,7 +2,6 @@ package tim2.CulturalHeritage.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static tim2.CulturalHeritage.constants.NewsConstants.*;
