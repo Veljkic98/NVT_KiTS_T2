9b6ccb5641f3f43d073826df2f2c45c1a2107ef4
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-25 13:09:18 +0100

    query writed un another way

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/NewsRepository.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/NewsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tim2.CulturalHeritage.model.News;
@@ -13,6 +14,6 @@ public interface NewsRepository extends JpaRepository<News, Long> {
 
     public Page<News> findAll(Pageable pageable);
 
-    @Query("select n from News n where n.culturalHeritage.id is ?1")
-    public Page<News> findAll(Pageable pageable, Long chID);
+    @Query("select n from News n where n.culturalHeritage.id = :chID")
+    public Page<News> findAll(Pageable pageable, @Param("chID") Long chID);
 }
