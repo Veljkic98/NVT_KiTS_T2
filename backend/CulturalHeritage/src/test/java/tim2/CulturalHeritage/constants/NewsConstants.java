2669ab60f874f9e6cf6686aae28a47882ff1453b
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-25 13:02:31 +0100

    updated constants

---
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/constants/NewsConstants.java
@@ -0,0 +1,13 @@
+package tim2.CulturalHeritage.constants;
+
+public class NewsConstants {
+
+    public static final Long CH_ID = 1L;
+    public static final Long CH_ID_NOT_EXISTS = 6897L;
+    public static final int PAGE_SIZE = 5;
+    // public static final
+    // public static final
+    // public static final
+    // public static final
+    
+}
