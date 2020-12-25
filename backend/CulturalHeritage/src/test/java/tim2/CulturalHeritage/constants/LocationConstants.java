513e2076293851dfaffe2df3323fa9aa9bbfd819
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-25 22:58:46 +0100

    location constants edited

--- backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/constants/LocationConstants.java
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/constants/LocationConstants.java
@@ -4,10 +4,13 @@ public class LocationConstants {
 
     public static final Long ID_NOT_EXISTS = 19632L;
     public static final Long ID_IN_USE = 1L;
-    public static final Long LOCATION_ID_FOR_UPDATEING = 2L;
-    public static final Long LOCATION_ID_IN_USE_FOR_UPDATEING = 2L;
+    public static final Long ID_NOT_IN_USE = 2L;
+    public static final Long LOCATION_ID_IN_USE_FOR_UPDATEING = 1L;
     public static final String LATITUDE = "123";
     public static final String LONGITUDE = "321";
     public static final int PAGE_SIZE = 5;
     public static final int LOCATIONS_NUMBER = 2;
+    public static final String COUNTRY = "country 1";
+    public static final String CITY = "city 1";
+    public static final String STREET = "street 1";
 }
