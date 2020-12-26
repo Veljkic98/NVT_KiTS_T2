34147778df4470a6180d190474dc8dbdd9d6e291
specnazm <laketic.milena98@gmail.com>
2020-12-26 10:10:54 +0100

    Added test for controller layer

---
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/controller/AuthUserControllerIntegrationTest.java
@@ -0,0 +1,97 @@
+package tim2.CulturalHeritage.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import tim2.CulturalHeritage.dto.requestDTO.AuthUserRequestDTO;
+import tim2.CulturalHeritage.dto.responseDTO.AuthUserResponseDTO;
+import tim2.CulturalHeritage.service.AuthenticatedUserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static tim2.CulturalHeritage.constants.AuthUserConstants.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class AuthUserControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private AuthenticatedUserService authenticatedUserService;
+
+    private HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    public void testGetOne() {
+        HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<AuthUserResponseDTO> responseEntity =
+                restTemplate.exchange("/api/authenticated-users/" + EXIST_USER_ID, HttpMethod.GET, httpEntity,
+                        AuthUserResponseDTO.class);
+
+        AuthUserResponseDTO user = responseEntity.getBody();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(user);
+    }
+
+    @Test
+    public void testGetOneInvaliID() {
+        HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<AuthUserResponseDTO> responseEntity =
+                restTemplate.exchange("/api/authenticated-users/" + NONEXIST_USER_ID, HttpMethod.GET, httpEntity,
+                        AuthUserResponseDTO.class);
+
+        AuthUserResponseDTO user = responseEntity.getBody();
+
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertNull(user);
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void testCreateUserValid() {
+        AuthUserRequestDTO userRequest = new AuthUserRequestDTO(NEW_FIRST_NAME, NEW_LAST_NAME, NEW_EMAIL, NEW_PASSWORD);
+
+        HttpEntity<Object> httpEntity = new HttpEntity<>(userRequest, headers);
+
+        ResponseEntity<AuthUserResponseDTO> responseEntity =
+                restTemplate.exchange("/api/authenticated-users/" , HttpMethod.POST, httpEntity,
+                        AuthUserResponseDTO.class);
+
+        AuthUserResponseDTO user = responseEntity.getBody();
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(NEW_EMAIL, user.getEmail());
+        assertEquals(NEW_FIRST_NAME, user.getFirstName());
+        assertEquals(NEW_LAST_NAME, user.getLastName());
+        assertNotNull(user.getId());
+        assertTrue(!user.isApproved());
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void testCreateUserInvalidEmail() {
+        AuthUserRequestDTO userRequest = new AuthUserRequestDTO(NEW_FIRST_NAME, NEW_LAST_NAME, EXIST_EMAIL, NEW_PASSWORD);
+
+        HttpEntity<Object> httpEntity = new HttpEntity<>(userRequest, headers);
+
+        ResponseEntity<AuthUserResponseDTO> responseEntity =
+                restTemplate.exchange("/api/authenticated-users/" , HttpMethod.POST, httpEntity,
+                        AuthUserResponseDTO.class);
+
+        AuthUserResponseDTO user = responseEntity.getBody();
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertNull(user.getId());
+    }
+}
