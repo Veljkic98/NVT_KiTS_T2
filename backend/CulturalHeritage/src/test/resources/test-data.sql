47c74569388dd98fc8d7d6e90ea7d4a583cbabe3
specnazm <laketic.milena98@gmail.com>
2020-12-30 13:57:49 +0100

    Fixed sql files

--- backend/CulturalHeritage/src/test/resources/test-data.sql
+++ backend/CulturalHeritage/src/test/resources/test-data.sql
@@ -14,38 +14,33 @@ INSERT INTO cultural_heritage (name, location_id, chsubtype_id, description) VAL
 INSERT INTO cultural_heritage (name, location_id, chsubtype_id, description) VALUES ('Guca Festival', 3, 2, 'The Guca festival...');
 
 
---AUTHORITY
 INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
 INSERT INTO authority (name) VALUES ('ROLE_USER');
 
---ADMIN
 INSERT INTO admin (id, first_name, last_name, email, password, approved) VALUES (1,'John', 'Smith', 'admin@gmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', 'true');
 insert into user_authority (user_id, authority_id) values (1, 1);
 
 
---USERS  SIZE = 4
 INSERT INTO authenticated_user (id, first_name, last_name, email, password, approved) VALUES (2, 'Helen', 'York', 'helen@hotmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', 'true');
 insert into user_authority (user_id, authority_id) values (2, 2);
 
 INSERT INTO authenticated_user (id, first_name, last_name, email, password, approved) VALUES (3,'Sima', 'Matas', 'sima12@hotmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', 'true');
-insert into user_authority (user_id, authority_id) values (3, 2)
+insert into user_authority (user_id, authority_id) values (3, 2);
 
 INSERT INTO authenticated_user (id, first_name, last_name, email, password, approved) VALUES (4,'Margene', 'Weatherwax', 'Weatherwax12@gmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', 'true');
-insert into user_authority (user_id, authority_id) values (4, 2)
+insert into user_authority (user_id, authority_id) values (4, 2);
 
 INSERT INTO authenticated_user (id, first_name, last_name, email, password, approved) VALUES (5,'Everette', 'Desch', 'heldesch123en@hotmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', 'false');
-insert into user_authority (user_id, authority_id) values (5, 2)
+insert into user_authority (user_id, authority_id) values (5, 2);
 
 
 insert into news (content, heading, cultural_heritage_id, admin_id) values ('sadrzajj', 'Naslov1', 1, 1);
 insert into news (content, heading, cultural_heritage_id, admin_id) values ('sadrzajj2', 'Naslov2', 1, 1);
 insert into news (content, heading, cultural_heritage_id, admin_id) values ('sadrzajj3', 'Naslov3', 1, 1);
 
---RATINGS  SIZE = 3  (2 grades for same ch, one for another)
 INSERT INTO rating (grade, authenticated_user_id, cultural_heritage_id) VALUES (3, 3, 1);
 INSERT INTO rating (grade, authenticated_user_id, cultural_heritage_id) VALUES (4, 4, 1);
 INSERT INTO rating (grade, authenticated_user_id, cultural_heritage_id) VALUES (5, 2, 2);
 
---COMMENTS
 insert into comment (cultural_heritage_id, authenticated_user_id, content) values (1, 4, 'Duis at velit eu est congue elementum. In hac habitasse platea dictumst.');
 insert into comment (cultural_heritage_id, authenticated_user_id, content) values (1, 3, 'Duis bibendum.');
