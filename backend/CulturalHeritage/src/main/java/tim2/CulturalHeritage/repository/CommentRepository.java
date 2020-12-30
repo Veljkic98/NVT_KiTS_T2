3837be121e1cc21c6fea329b0166eef97ccb4c66
specnazm <laketic.milena98@gmail.com>
2020-12-30 13:51:38 +0100

    Get all comments with paggination for chosen ch

--- backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/CommentRepository.java
+++ backend/CulturalHeritage/src/main/java/tim2/CulturalHeritage/repository/CommentRepository.java
@@ -3,11 +3,17 @@ package tim2.CulturalHeritage.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tim2.CulturalHeritage.model.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
     Page<Comment> findAll(Pageable pageable);
+
+    @Query("select c from Comment c where c.culturalHeritage.id = :chID")
+    public Page<Comment> findAll(Pageable pageable, @Param("chID") Long chID);
 }
