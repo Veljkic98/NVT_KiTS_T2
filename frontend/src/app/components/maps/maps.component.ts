aa4a3e6a763a6426ae11352e74b0ff3424a12325
alekalek <alek@spectra-alarm.com>
2021-01-16 23:06:58 +0100

    added addMarker function. Added colors array for markers. Added consoleLogColors for debugging

--- frontend/src/app/components/maps/maps.component.ts
+++ frontend/src/app/components/maps/maps.component.ts
@@ -1,5 +1,7 @@
+import { animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import {Map, Marker} from 'mapbox-gl';
+
 
 @Component({
   selector: 'app-maps',
@@ -15,20 +17,42 @@ export class MapsComponent implements OnInit {
   maxBounds: number[][] =[[-180,-85], [180,85]];
   style: string = 'mapbox://styles/tim2/ckjgzxl2koofq19qkxsa2ogt0';
   map: Map;
+  markerIcon: HTMLDivElement;
+  iconColors: string[];
   
   constructor() { }
 
   ngOnInit(): void {
-
-    setTimeout( ()=> {
-      
-    }, 5000)
-    
+    this.iconColors =  ["Teal", "Lavender", "PaleVioletRed", "LightSalmon", "Orange", "YellowGreen", "Indigo", "Silver", "Moccasin", "DarkBlue", "Bisque", "SeaGreen", "LightSlateGray", "DarkKhaki", "DarkViolet", "MediumPurple", "DarkSlateGray", "DarkOrange", "LightGrey", "Linen", "DarkSalmon", "PaleGoldenRod", "HoneyDew", "Grey", "LightYellow", "SaddleBrown", "DarkGray", "Cornsilk", "SlateGray", "Chocolate", "Turquoise", "PowderBlue", "Gold", "MidnightBlue", "SpringGreen", "OldLace", "Peru", "MediumAquaMarine", "Blue", "White", "FireBrick", "LemonChiffon", "BurlyWood", "AliceBlue", "Red", "Aquamarine", "LightCyan", "MediumSpringGreen", "Fuchsia"];
+    this.consoleLogColors();
   }
 
   onMapLoad(map: Map){
     this.map = map;
-    let marker = new Marker().setLngLat(this.center).addTo(this.map);
+
+    this.addMarker(this.center, this.iconColors[3]);
   }
+
+  addMarker(coordinates, color="red"){
+    this.markerIcon = document.createElement('div');
+
+    //creating this: <i class="material-icons">place</i>
+    let icon = document.createElement('i');             //html elem name
+    icon.classList.add('material-icons');               //class name
+    icon.style.fontSize = "40px";                       //font size
+    icon.appendChild(document.createTextNode("place")); //google icon name
+    icon.style.color = color;                            //marker color
+    this.markerIcon.appendChild(icon);
+    let marker = new Marker(this.markerIcon).setLngLat(coordinates).addTo(this.map);
+  }
+
+  //this is only for debuging so you can see colors array
+  consoleLogColors(){
+    for(let i = 0; i < this.iconColors.length; i++){
+      // console.log(`%c ${this.iconColors[i]}`, `color: ${this.iconColors[i]}`);
+      console.log(`%c     `, `background-color: ${this.iconColors[i]}`);
+  }
+  }
+
 
 }
