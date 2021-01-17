e842e743e6f63f2602e4ef524a4e550e1cc8c4ac
alekalek <alek@spectra-alarm.com>
2021-01-17 11:10:05 +0100

    added get pageable for ch

--- frontend/src/app/components/maps/maps.component.ts
+++ frontend/src/app/components/maps/maps.component.ts
@@ -1,7 +1,7 @@
-import { animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import {Map, Marker} from 'mapbox-gl';
-
+import {CulturalHeritageService} from '../../services/cultural-heritage-serice/cultural-heritage.service';
+import {CulturalHeritage} from '../../models/cultural-heritage.model'
 
 @Component({
   selector: 'app-maps',
@@ -9,7 +9,7 @@ import {Map, Marker} from 'mapbox-gl';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  //default values
+  //default map values
   zoom: number = 5;
   latitude: number = 47;
   longitude: number = 13;
@@ -19,12 +19,19 @@ export class MapsComponent implements OnInit {
   map: Map;
   markerIcon: HTMLDivElement;
   iconColors: string[];
+  //CulturalHeritages
+  culturalHeritage: CulturalHeritage;
   
-  constructor() { }
+  constructor(private culturalHeritageService: CulturalHeritageService) { }
 
   ngOnInit(): void {
     this.iconColors =  ["Teal", "Lavender", "PaleVioletRed", "LightSalmon", "Orange", "YellowGreen", "Indigo", "Silver", "Moccasin", "DarkBlue", "Bisque", "SeaGreen", "LightSlateGray", "DarkKhaki", "DarkViolet", "MediumPurple", "DarkSlateGray", "DarkOrange", "LightGrey", "Linen", "DarkSalmon", "PaleGoldenRod", "HoneyDew", "Grey", "LightYellow", "SaddleBrown", "DarkGray", "Cornsilk", "SlateGray", "Chocolate", "Turquoise", "PowderBlue", "Gold", "MidnightBlue", "SpringGreen", "OldLace", "Peru", "MediumAquaMarine", "Blue", "White", "FireBrick", "LemonChiffon", "BurlyWood", "AliceBlue", "Red", "Aquamarine", "LightCyan", "MediumSpringGreen", "Fuchsia"];
-    this.consoleLogColors();
+    // this.consoleLogColors();
+    this.getCH();
+
+  }
+  getCH(){
+    this.culturalHeritageService.getCHCoordinates(0);
   }
 
   onMapLoad(map: Map){
@@ -51,7 +58,7 @@ export class MapsComponent implements OnInit {
     for(let i = 0; i < this.iconColors.length; i++){
       // console.log(`%c ${this.iconColors[i]}`, `color: ${this.iconColors[i]}`);
       console.log(`%c     `, `background-color: ${this.iconColors[i]}`);
-  }
+    }
   }
 
 
