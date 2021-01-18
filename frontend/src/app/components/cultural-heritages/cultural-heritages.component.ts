f69169d9c6e55c651056e5faca55d2a3667a25c2
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-01-18 23:19:42 +0100

    S2-1-T2 done. Created table with all cs with pagination.

--- frontend/src/app/components/cultural-heritages/cultural-heritages.component.ts
+++ frontend/src/app/components/cultural-heritages/cultural-heritages.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 import { CulturalHeritageService } from 'src/app/services/cultural-heritage-service/cultural-heritage.service';
-import { CulturalHeritage } from 'src/app/models/cultural-heritage.model';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
 
 export interface PeriodicElement {
   name: string;
@@ -14,16 +10,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -33,31 +29,49 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CulturalHeritagesComponent implements OnInit { // AfterViewInit 
 
-  page = 1;
+  totalPages = 0;
+  length = 0;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+  showFirstLastButtons = true;
+  
   error: string;
 
-  displayedColumns: string[] = ['name', 'subtypeName', 'totalRatings'];
+  displayedColumns: string[] = ['name', 'subtypeName', 'totalRatings', 'actions'];
   dataSource = [];
+
+  pageEvent: PageEvent;
 
   constructor(
     private service: CulturalHeritageService
   ) { }
 
   ngOnInit(): void {
-    this.getCulturalHeritages(this.page);
+    this.getCulturalHeritages(this.pageIndex, this.pageSize);
   }
 
-  getCulturalHeritages(page: number): void {
-    this.service.getCulturalHeritages(page - 1).subscribe(
+  getCulturalHeritages(page: number, size: number): void {
+    this.service.getCulturalHeritagesWithSize(page, size).subscribe(
       data => {
         console.log(data.content)
         this.dataSource = data.content;
+        this.length = data.totalElements;
+        this.totalPages = data.totalPages;
       },
       error => {
         console.log(error);
         this.error = 'Can\'t load cultural heritages for some reason';
       }
     );
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.length = event.length;
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+
+    this.getCulturalHeritages(this.pageIndex, this.pageSize);
   }
 
 }
--- frontend/src/app/services/cultural-heritage-service/cultural-heritage.service.ts
+++ frontend/src/app/services/cultural-heritage-service/cultural-heritage.service.ts
@@ -6,8 +6,8 @@ import { Page } from '../../models/page.model'
 import { Observable } from 'rxjs';
 
 const REST_ENDPOINT = {
-    getOne: '/cultural-heritages/',
-    getByPage: '/cultural-heritages/by-page',
+  getOne: '/cultural-heritages/',
+  getByPage: '/cultural-heritages/by-page',
 }
 
 import { CULTURAL_HERITAGES } from '../../utils/constants';
@@ -23,7 +23,11 @@ export class CulturalHeritageService {
     return this.http.get<Page>(`${environment.apiUrl}${REST_ENDPOINT.getByPage}/?page=${page}&size=10`);
   }
 
-    getOne(id:number): Observable<CulturalHeritage> {
-        return this.http.get<CulturalHeritage>(`${environment.apiUrl}/${CULTURAL_HERITAGES}/${id}`);
-    }
+  getCulturalHeritagesWithSize(pageIndex: number, pageSize: number): Observable<Page> {
+    return this.http.get<Page>(`${environment.apiUrl}${REST_ENDPOINT.getByPage}/?page=${pageIndex}&size=${pageSize}`);
+  }
+
+  getOne(id: number): Observable<CulturalHeritage> {
+    return this.http.get<CulturalHeritage>(`${environment.apiUrl}/${CULTURAL_HERITAGES}/${id}`);
+  }
 }
