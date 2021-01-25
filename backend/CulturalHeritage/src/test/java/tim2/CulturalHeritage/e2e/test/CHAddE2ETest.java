29a86d45d900c1eb0c848d570fbfe881cd454b5e
alekalek <alek@spectra-alarm.com>
2021-01-25 11:51:03 +0100

    add button test for ch done

---
+++ backend/CulturalHeritage/src/test/java/tim2/CulturalHeritage/e2e/test/CHAddE2ETest.java
@@ -0,0 +1,72 @@
+package tim2.CulturalHeritage.e2e.test;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import tim2.CulturalHeritage.e2e.page.CHAddPage;
+import tim2.CulturalHeritage.e2e.page.LoginPage;
+
+public class CHAddE2ETest {
+  private WebDriver driver;
+
+  private CHAddPage chAddPage;
+
+  private LoginPage loginPage;
+
+  @Before
+  public void setUp() {
+
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    driver = new ChromeDriver();
+
+    driver.manage().window().maximize();
+    chAddPage = PageFactory.initElements(driver, CHAddPage.class);
+    loginPage = PageFactory.initElements(driver, LoginPage.class);
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+
+  private void justWait(Integer howLong) throws InterruptedException {
+    synchronized (driver) {
+      driver.wait(howLong);
+    }
+  }
+
+  public void logInUser() throws InterruptedException {
+    driver.get("http://localhost:4200/login");
+
+    loginPage.getEmail().sendKeys("sima12@hotmail.com");
+    loginPage.getPassword().sendKeys("123");
+    loginPage.getLoginBtn().click();
+    justWait(1500);
+  }
+
+  public void logInAdmin() throws InterruptedException {
+    driver.get("http://localhost:4200/login");
+
+    loginPage.getEmail().sendKeys("admin@gmail.com");
+    loginPage.getPassword().sendKeys("123");
+    loginPage.getLoginBtn().click();
+    justWait(1500);
+  }
+
+  @Test
+  public void clickAddButton() throws InterruptedException{
+    logInAdmin();
+    driver.get("http://localhost:4200/cultural-heritages");
+    justWait(1000);
+    chAddPage.ensureAddButtonIsPresent();
+    justWait(1000);
+    chAddPage.getAddButton().click();
+    justWait(1000);
+  }
+}
