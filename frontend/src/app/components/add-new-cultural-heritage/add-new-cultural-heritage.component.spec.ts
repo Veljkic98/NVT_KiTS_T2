bd7af2b26215859054e47f4ad363e507ef5c4217
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-01-26 23:04:16 +0100

    add new CH angular testing done.

--- frontend/src/app/components/add-new-cultural-heritage/add-new-cultural-heritage.component.spec.ts
+++ frontend/src/app/components/add-new-cultural-heritage/add-new-cultural-heritage.component.spec.ts
@@ -2,20 +2,28 @@ import { Overlay } from '@angular/cdk/overlay';
 import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { of } from 'rxjs';
-import { CHSubtype } from 'src/app/models/ch-subtype.model';
+import { CHSubtype, CHSubtype2, CHSubtype3 } from 'src/app/models/ch-subtype.model';
+import { Location } from 'src/app/models/location.model';
 import { CulturalHeritage } from 'src/app/models/cultural-heritage.model';
 import { CHSubtypeService } from 'src/app/services/ch-subtype-service/ch-subtype.service';
 import { CulturalHeritageService } from 'src/app/services/cultural-heritage-service/cultural-heritage.service';
+import { LocationService } from 'src/app/services/location/location.service';
 
 import { AddNewCulturalHeritageComponent } from './add-new-cultural-heritage.component';
+import { CulturalHeritagesComponent } from '../cultural-heritages/cultural-heritages.component';
 
 describe('AddNewCulturalHeritageComponent', () => {
   let component: AddNewCulturalHeritageComponent;
   let fixture: ComponentFixture<AddNewCulturalHeritageComponent>;
   let chService: CulturalHeritageService;
   let subtypeService: CHSubtypeService;
+  let locationService: LocationService;
+  let router: Router;
+  let route: ActivatedRoute;
 
   beforeEach(async () => {
 
@@ -41,6 +49,11 @@ describe('AddNewCulturalHeritageComponent', () => {
         locationName: "lokacija1",
       })
 
+    const l = new Location("12", "12", "asd", "asd", "asd");
+    l.id = 1;
+
+    const mockLocation = l;
+
     const mockSubtypes = [
       new CHSubtype({
         id: 1,
@@ -56,19 +69,8 @@ describe('AddNewCulturalHeritageComponent', () => {
 
     let subtypeServiceMock = {
       getAll: jasmine.createSpy('getAll')
-        .and.returnValue(of({
-          content: mockSubtypes,
-          // empty: false,
-          // first: true,
-          // last: true,
-          // number: 0,
-          // numberOfElements: 2,
-          // pageable: { sort: { sorted: true, unsorted: false, empty: false }, offset: 0, pageNumber: 0, pageSize: 2 },
-          // size: 2,
-          // sort: { sorted: true, unsorted: false, empty: false },
-          // totalElements: 2,
-          // totalPages: 1
-        }
+        .and.returnValue(of(
+          mockSubtypes
         )),
     }
 
@@ -77,15 +79,24 @@ describe('AddNewCulturalHeritageComponent', () => {
         .and.returnValue(of(mockCulturalHeritage)),
     }
 
+    let locationServiceMock = {
+      post: jasmine.createSpy('post')
+        .and.returnValue(of(mockLocation)),
+    }
+
     await TestBed.configureTestingModule({
       declarations: [AddNewCulturalHeritageComponent],
       providers: [
         { provide: CulturalHeritageService, useValue: chServiceMock },
         { provide: CHSubtypeService, useValue: subtypeServiceMock },
+        { provide: LocationService, useValue: locationServiceMock },
         MatSnackBar, Overlay, NgbModal, HttpClient
       ],
       imports:
         [
+          RouterTestingModule.withRoutes([{ path: 'cultural-heritages', component: CulturalHeritagesComponent },]),
+          // RouterTestingModule
+          // Router
           // HttpClient
           // NgxPaginationModule,
           // BrowserAnimationsModule
@@ -99,6 +110,8 @@ describe('AddNewCulturalHeritageComponent', () => {
     component = fixture.componentInstance;
     chService = TestBed.inject(CulturalHeritageService);
     subtypeService = TestBed.inject(CHSubtypeService);
+    locationService = TestBed.inject(LocationService);
+    router = TestBed.inject(Router);
     // expect(component.dataSource).toEqual([]);
     // expect(component.pageIndex).toBe(0);
     fixture.detectChanges();
@@ -109,44 +122,35 @@ describe('AddNewCulturalHeritageComponent', () => {
   });
 
   fdescribe('ngOnInit()', () => {
-    it('should fetch all subtypes on init (with paggination)', fakeAsync(() => {
+    it('should fetch all subtypes on init', fakeAsync(() => {
       // fixture.detectChanges();
       component.ngOnInit();
-      expect(subtypeService.getAll).toHaveBeenCalledWith(); // page index is 0 
+      expect(subtypeService.getAll).toHaveBeenCalled();
       // tick();
 
       expect(component.subtypes.length).toBe(2);
     }));
   })
 
-  // fdescribe('addCH()', () => {
-  //   it('should add', fakeAsync(() => {
+  fdescribe('addCH()', () => {
+    it('should add', fakeAsync(() => {
 
-  //     // component.ngOnInit();
+      // spyOn(component, '');
+      const navigateSpy = spyOn(router, 'navigate');
 
-  //     tick();
+      component.location = new Location("12", "12", "asd", "asd", "asd");
+      component.location.id = 1;
 
-  //     // fixture.detectChanges();
+      component.name = "naziv1";
+      component.description = "opis1";
+      component.url = "asdasdasdad";
+      component.subtype = new CHSubtype2();
+      component.subtype.id = 1;
 
-  //     component.name = "naziv1";
-  //     component.description = "opis1";
-  //     component.isSubtypeChosen = true;
-  //     component.isFileChosen = true;
-  //     // component.location = new Location({});
-  //     component.url = "asdasd"
-  //     // fixture.detectChanges();
-      
-      
-  //     // component.addCH();
-  //     // flush();
+      component.addCH();
+      expect(locationService.post).toHaveBeenCalledWith(component.location);
 
-  //     // expect(chService.delete).toHaveBeenCalledWith(2);
-
-  //     // fixture.detectChanges();
-
-  //     // let comments: DebugElement[] = fixture.debugElement.queryAll(By.css('.comment-root'));
-  //     // expect(comments.length).toBe(1);
-
-  //   }));
-  // })
+      expect(navigateSpy).toHaveBeenCalledWith(['/cultural-heritages'])
+    }));
+  })
 });
--- frontend/src/app/components/cultural-heritage/cultural-heritage.component.spec.ts
+++ frontend/src/app/components/cultural-heritage/cultural-heritage.component.spec.ts
@@ -17,39 +17,41 @@ describe('CulturalHeritageComponent', () => {
 
   beforeEach(async () => {
     const authServiceMock = {
-        getRole: jasmine.createSpy('getRole')
+      getRole: jasmine.createSpy('getRole')
         .and.returnValue(of('USER')),
 
-        getSubscriptions: jasmine.createSpy('getSubscriptions')
+      getSubscriptions: jasmine.createSpy('getSubscriptions')
         .and.returnValue(of([]))
-      };
+    };
     const chServiceMock = {
-    getOne: jasmine.createSpy('getOne')
-    .and.returnValue(of(new CulturalHeritage({
-      id: 1,
-      avgRating: 0,
-      chsubtypeID: 1,
-      coordinates:  ['12.327145', '45.438759'],
-      description: 'The Carnival of Venice (Italian: Carnevale di Venezia) is an annual festival, held in Venice, Italy. The Carnival starts forty days before Easter and ends on Shrove Tuesday (Fat Tuesday or MartedÃ¬ Grasso), the day before Ash Wednesday. Dove il gabinetto! In other words, At a carnival, every joke is disgraced!',
-      imageUri: 'http://localhost:8080/api/files/1',
-      locationID: 1,
-      locationName: 'Italy Venice',
-      name: 'Venice Carnival',
-      totalRatings: 0
-    })))
+      getOne: jasmine.createSpy('getOne')
+        .and.returnValue(of(new CulturalHeritage({
+          id: 1,
+          avgRating: 0,
+          chsubtypeID: 1,
+          coordinates: ['12.327145', '45.438759'],
+          description: 'The Carnival of Venice (Italian: Carnevale di Venezia) is an annual festival, held in Venice, Italy. The Carnival starts forty days before Easter and ends on Shrove Tuesday (Fat Tuesday or MartedÃ¬ Grasso), the day before Ash Wednesday. Dove il gabinetto! In other words, At a carnival, every joke is disgraced!',
+          imageUri: 'http://localhost:8080/api/files/1',
+          locationID: 1,
+          locationName: 'Italy Venice',
+          name: 'Venice Carnival',
+          totalRatings: 0
+        }))),
+        subscribe: jasmine.createSpy('subscribe')
+        .and.returnValue
     };
     const commentServiceMock = {
 
     };
     await TestBed.configureTestingModule({
-      declarations: [ CulturalHeritageComponent ],
-      providers:    [
-        {provide: AuthService, useValue: authServiceMock },
-        {provide: CulturalHeritageService, useValue: chServiceMock },
+      declarations: [CulturalHeritageComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: CulturalHeritageService, useValue: chServiceMock },
         MatSnackBar, Overlay
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -68,32 +70,44 @@ describe('CulturalHeritageComponent', () => {
   });
 
   describe('ngOnInit()', () => {
-  it('should fetch cultural heritage, user rating for it and subsctiption on init ', fakeAsync(() => {
-    component.ngOnInit();
-    expect(chService.getOne).toHaveBeenCalled();
-    tick();
+    it('should fetch cultural heritage, user rating for it and subsctiption on init ', fakeAsync(() => {
+      component.ngOnInit();
+      expect(chService.getOne).toHaveBeenCalled();
+      tick();
 
-    expect(component.ch.id).toBe(1);
-    expect(component.ch.name).toEqual('Venice Carnival');
-    expect(component.ch.locationName).toEqual('Italy Venice');
-    expect(component.ch.imageUri).toEqual('http://localhost:8080/api/files/1');
-    expect(component.ch.totalRatings).toEqual(0);
-    expect(component.ch.coordinates).toEqual(['12.327145', '45.438759']);
 
-    expect(component.isSubscribed).toBe(false);
-    expect(component.error).toBe(null);
+      expect(component.ch.id).toBe(1);
+      expect(component.ch.name).toEqual('Venice Carnival');
+      expect(component.ch.locationName).toEqual('Italy Venice');
+      expect(component.ch.imageUri).toEqual('http://localhost:8080/api/files/1');
+      expect(component.ch.totalRatings).toEqual(0);
+      expect(component.ch.coordinates).toEqual(['12.327145', '45.438759']);
 
-    // //should display fetched cultural heritage
-    fixture.detectChanges();
-    tick();
-    fixture.detectChanges();
+      expect(component.isSubscribed).toBe(false);
+      expect(component.error).toBe(null);
 
-    const cardTitle = fixture.debugElement.query(By.css('mat-card-title')).nativeElement;
-    expect(cardTitle.textContent).toEqual('Venice Carnival');
-    const cardSubTitle = fixture.debugElement.query(By.css('mat-card-subtitle')).nativeElement;
-    expect(cardSubTitle.textContent).toEqual('Italy Venice');
-    const rating = fixture.debugElement.query(By.css('.rate')).nativeElement;
-    expect(rating.textContent).toEqual(' 0 ');
-  }));
-});
+      // //should display fetched cultural heritage
+      fixture.detectChanges();
+      tick();
+      fixture.detectChanges();
+
+      const cardTitle = fixture.debugElement.query(By.css('mat-card-title')).nativeElement;
+      expect(cardTitle.textContent).toEqual('Venice Carnival');
+      const cardSubTitle = fixture.debugElement.query(By.css('mat-card-subtitle')).nativeElement;
+      expect(cardSubTitle.textContent).toEqual('Italy Venice');
+      const rating = fixture.debugElement.query(By.css('.rate')).nativeElement;
+      expect(rating.textContent).toEqual(' 0 ');
+    }));
+  });
+
+  describe('subscribe()', () => {
+    it('should subscribe user to CH.', fakeAsync(() => {
+      component.chID = 1;
+      // component.ngOnInit();
+      tick();
+      component.subscribe();
+      tick();
+
+    }));
+  });
 });
