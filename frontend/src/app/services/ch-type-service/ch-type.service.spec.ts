0e8fa0e83d74f697987042b28d1fd2e21393f54d
alekalek <alek@spectra-alarm.com>
2021-01-27 16:30:21 +0100

    added add type service test

--- frontend/src/app/services/ch-type-service/ch-type.service.spec.ts
+++ frontend/src/app/services/ch-type-service/ch-type.service.spec.ts
@@ -1,22 +1,22 @@
 import { TestBed, getTestBed, inject } from '@angular/core/testing';
 import { CHTypeService } from './ch-type.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {fakeAsync, tick} from '@angular/core/testing';
+import { fakeAsync, tick } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
 import { CHType } from 'src/app/models/ch-type.model';
 import { Page } from 'src/app/models/page.model';
 
-describe('CHTypeService', () => {
+fdescribe('CHTypeService', () => {
   let injector;
   let chTypeService: CHTypeService;
   let httpMock: HttpTestingController;
   let httpClient: HttpClient;
 
-	 beforeEach(() => {
+  beforeEach(() => {
 
     TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        providers: [CHTypeService]
+      imports: [HttpClientTestingModule],
+      providers: [CHTypeService]
     });
 
     injector = getTestBed();
@@ -30,9 +30,9 @@ describe('CHTypeService', () => {
   });
 
 
- 	it('should pass simple test', () => {
-	    expect(true).toBe(true);
-    });
+  it('should pass simple test', () => {
+    expect(true).toBe(true);
+  });
 
 
 
@@ -73,12 +73,12 @@ describe('CHTypeService', () => {
     );
 
     chTypeService.getTypes(0).subscribe(data => {
-            types = data.content;
-            numberOfElements = data.numberOfElements;
-            size = data.size;
-            totalElements = data.totalElements;
-            totalPages = data.totalPages;
-            last = data.last;
+      types = data.content;
+      numberOfElements = data.numberOfElements;
+      size = data.size;
+      totalElements = data.totalElements;
+      totalPages = data.totalPages;
+      last = data.last;
     });
 
     const req = httpMock.expectOne('http://localhost:8080/api/ch-types/by-page?page=0&size=10&sort=id,ASC');
@@ -107,42 +107,67 @@ describe('CHTypeService', () => {
 
 
   it('deleteType() should query url and delete type', fakeAsync(() => {
-        chTypeService.deleteType(2).subscribe(res => { });
+    chTypeService.deleteType(2).subscribe(res => { });
 
-        const req = httpMock.expectOne('http://localhost:8080/api/ch-types/2');
-        expect(req.request.method).toBe('DELETE');
-        req.flush({});
+    const req = httpMock.expectOne('http://localhost:8080/api/ch-types/2');
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
 
-    }));
+  }));
 
   it('editType() should query url and change existing type', fakeAsync(() => {
-        let type: CHType = new CHType({
-            id: 1,
-           name: 'manifestation',
-           subtypes : [
-               {id: 9, name: 'CAN BE DELETED', chTypeID: 1},
-               {id: 2, name: 'fair', chTypeID: 1},
-               {id: 1, name: 'festival', chTypeID: 1}
-           ]
-        });
+    let type: CHType = new CHType({
+      id: 1,
+      name: 'manifestation',
+      subtypes: [
+        { id: 9, name: 'CAN BE DELETED', chTypeID: 1 },
+        { id: 2, name: 'fair', chTypeID: 1 },
+        { id: 1, name: 'festival', chTypeID: 1 }
+      ]
+    });
 
-        const mockType: CHType = {
-            id: 2,
-            name: 'institution2',
-            subtypes: []
-        };
+    const mockType: CHType = {
+      id: 2,
+      name: 'institution2',
+      subtypes: []
+    };
 
-        chTypeService.editType(type).subscribe(res => type = res);
+    chTypeService.editType(type).subscribe(res => type = res);
 
-        const req = httpMock.expectOne('http://localhost:8080/api/ch-types/1');
-        expect(req.request.method).toBe('PUT');
-        req.flush(mockType);
+    const req = httpMock.expectOne('http://localhost:8080/api/ch-types/1');
+    expect(req.request.method).toBe('PUT');
+    req.flush(mockType);
 
-        tick();
+    tick();
 
-        expect(type).toBeDefined();
-        expect(type.id).toEqual(2);
-        expect(type.name).toEqual('institution2');
-      }));
+    expect(type).toBeDefined();
+    expect(type.id).toEqual(2);
+    expect(type.name).toEqual('institution2');
+  }));
+
+
+  it('addType() should post a type and return that type', fakeAsync(() => {
+    let type: CHType = new CHType({
+      name: 'televisa presenta',
+      subtypes: [ ]
+    });
+
+    const mockType: CHType = {
+      id: 4,
+      name: 'televisa presenta',
+      subtypes: []
+    };
+    chTypeService.addType(mockType).subscribe(res => type = res);
+
+    const req = httpMock.expectOne('http://localhost:8080/api/ch-types');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockType);
+
+    tick();
+
+    expect(type).toBeDefined();
+    expect(type.id).toEqual(4);
+    expect(type.name).toEqual('televisa presenta');
+  }));
 
 });
